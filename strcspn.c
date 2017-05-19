0b61ddf2b463312fe38c8218757a011d2ba56674
Anastasia Repina <anastasiya.repina2012@yandex.ru>
2017-05-19 15:19:49 +0300

    New version

--- strcspn.c
+++ strcspn.c
@@ -3,6 +3,7 @@
 /*@ requires valid_str(s);
 		requires valid_str(reject);
 		assigns \nothing;
+		ensures 0 <= \result <= strlen(s);
 	  ensures \forall char *t, integer i; 0 <= i < \result  && reject <= t < reject + strlen(reject)  ==> s[i] != *t;
 	  ensures \result == strcspn(s, reject);
  */
@@ -11,23 +12,23 @@ size_t strcspn(const char *s, const char *reject)
 	const char *p;
 	const char *r;
 	size_t count = 0;
-  /*@ loop invariant \base_addr(s) == \base_addr(p);
-	    loop invariant valid_str(p);
+  /*@ //loop invariant \base_addr(s) == \base_addr(p);
+	    //loop invariant valid_str(p);
+			loop invariant s <= p <= s + strlen(s);
       loop invariant 0 <= count <= strlen(s);
-			loop invariant count == p - s;
-      loop invariant s <= p <= s + strlen(s);
-      loop invariant strlen(s) == strlen(p) + (p - s);
-			//loop invariant \forall char *z, *t; s <= z < p && reject <= t < reject + strlen(reject) ==> *z != *t;
+			loop invariant count == p - s;      
+      //loop invariant strlen(s) == strlen(p) + (p - s);
+			loop invariant \forall char *z, *t; s <= z < p && reject <= t < reject + strlen(reject) ==> *z != *t;
 			//loop invariant strcspn(s, reject) >= strcspn(p, reject) + count;
-			loop assigns count, p, r;
+			//loop assigns count, p, r;
       loop variant strlen(s) - (p - s);
  */
 	for (p = s; *p != '\0'; ++p) {
-    /*@ loop invariant \base_addr(reject) == \base_addr(r);
-				loop invariant valid_str(r);
+    /*@ //loop invariant \base_addr(reject) == \base_addr(r);
+				//loop invariant valid_str(r);
 				loop invariant reject <= r <= reject + strlen(reject);
-				loop invariant strlen(reject) == strlen(r) + (r - reject);
-				//loop invariant \forall char *t; reject <= t < r ==> *p != *t;
+				//loop invariant strlen(reject) == strlen(r) + (r - reject);
+				loop invariant \forall char *t; reject <= t < r ==> *p != *t;
         loop variant strlen(reject) - (r - reject);
     */
 		for (r = reject; *r != '\0'; ++r) {
@@ -44,6 +45,6 @@ size_t strcspn(const char *s, const char *reject)
 
 int main(void)
 {
-    printf("%d\n", strcspn("123", "4"));
+    printf("%d\n", strcspn("12345", "43"));
     return 0;
 }
--- strcspn.h
+++ strcspn.h
@@ -12,41 +12,33 @@ typedef __kernel_size_t size_t;
     predicate valid_str(char *s) =
        \exists size_t n;
           s[n] == '\0' && \valid(s+(0..n));
-
     lemma valid_str_shift1:
        \forall char *s;
        *s != '\0' &&
        valid_str(s) ==> valid_str(s+1);
-
     logic size_t strlen(char *s) =
        s[0] == '\0' ? (size_t) 0 : (size_t) ((size_t)1 + strlen(s + 1));
-
     lemma strlen_before_null:
        \forall char* s, integer i;
           valid_str(s) &&
           0 <= i < strlen(s) ==> s[i] != '\0';
-
     lemma strlen_at_null:
        \forall char* s;
           valid_str(s) ==> s[strlen(s)] == '\0';
-
     lemma strlen_shift:
        \forall char *s, size_t i;
           valid_str(s) &&
           i <= strlen(s) ==>
           strlen(s+i) == strlen(s)-i;
-
     lemma strlen_shift_ex:
        \forall char *s, size_t i;
           valid_str(s) &&
           0 < i <= strlen(s) ==>
           strlen(s+i) < strlen(s);
-
     lemma strlen_shift1:
        \forall char *s;
           valid_str(s) && *s != '\0' ==>
           strlen(s) == 1 + strlen(s+1);
-
     lemma strlen_pointers:
        \forall char *s, *sc;
           valid_str(s)  &&
@@ -55,13 +47,13 @@ typedef __kernel_size_t size_t;
           s <= sc &&
           (\forall integer i; 0 <= i <= sc - s ==> s[i] != '\0') ==>
              strlen(sc) <= strlen(s);
-
     lemma strlen_main:
        \forall char *s, size_t n;
        valid_str(s) &&
        s[n] == '\0' &&
        (\forall size_t i; i < n ==> s[i] != '\0') ==>
            strlen(s) == n;
+    
 
    logic boolean in_array(char *s, char val) = *s == '\0' ? \false : (*s == val ? \true : in_array(s+1, val));
 
@@ -124,5 +116,5 @@ typedef __kernel_size_t size_t;
 
  	}
  */
- 
+
 #endif // __STRCSPN_H__
